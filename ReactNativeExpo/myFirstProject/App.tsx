52ee76a84c48988dc778b049189937ae17d9c7b2
Brenno Ysrael <brenno.maluf@gmail.com>
2025-02-25 23:20:39 -0400

    Atualizações de Projetos Expo + React Native

---
+++ ReactNativeExpo/myFirstProject/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textColor}>Hello World</Text>
+      <Text>Oi, eu sou o Goku!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textColor: {
+    color: 'purple',
+    fontSize: 60,
+  }
+});

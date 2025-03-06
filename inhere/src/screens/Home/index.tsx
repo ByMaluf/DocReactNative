fe1fec7ebe0cc4fb2fd97fb7a9b4769d75acf550
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-03-06 13:16:33 -0400

    Alertas no React Native

--- inhere/src/screens/Home/index.tsx
+++ inhere/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, TextInput, View, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity, ScrollView, FlatList, Alert } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
 
@@ -7,11 +7,23 @@ export default function Home() {
   const participants = ['Brenno', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Brunno', 'Brunna'];
 
   function handleParticipantAdd() {
-    console.log('Adicionou um participante');
+    if (participants.includes('Brenno')) {
+      Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome.');
+      return;
+    }
   }
 
   function handleParticipantRemove(name: string) {
-    console.log(`Removeu participante ${name}`)
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert(`${name} foi deletado!`)
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
   }
 
   return (

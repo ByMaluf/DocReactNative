775dc15fee9bffc7f43dd82b4143fe9e54e5a995
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-03-13 17:22:09 -0400

    Atualizações

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -2,14 +2,13 @@ import { Container } from './style';
 import { Header } from '@components/Header';
 import HighLight from '@components/Highlight';
 import GroupCard from '@components/GroupCard';
+import ListEmpty from '@components/ListEmpty';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
 export function Groups() {
 
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', 'Família']);
-
-
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -22,6 +21,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard
             title={item} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
+          />
         )}
       />
 

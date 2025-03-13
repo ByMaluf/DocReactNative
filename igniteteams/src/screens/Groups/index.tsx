003765208888e91a7ff15097b2a0c98e0decf1d2
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-03-13 10:10:33 -0400

    Listagem de Grupos

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,15 +1,30 @@
-import { Text } from 'react-native';
 import { Container } from './style';
 import { Header } from '@components/Header';
 import HighLight from '@components/Highlight';
 import GroupCard from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', 'Família']);
+
+
+
   return (
     <Container>
       <Header />
       <HighLight title='Turmas' subtitle='Jogue com a sua turma' />
-      <GroupCard title='Galera do Ignite' onPress={() => ''} />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item} />
+        )}
+      />
+
     </Container>
   );
 }

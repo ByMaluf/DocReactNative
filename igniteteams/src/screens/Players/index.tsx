44e3c0c4847a603eb994f0e852f3a2466f7d830d
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-03-17 12:17:53 -0400

    Filter + Players

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -1,10 +1,17 @@
-import { Container, Form } from "./style";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./style";
 import { Header } from "@components/Header";
+import { Filter } from "@components/Filter";
 import { ButtonIcon } from "@components/ButtonIcon";
 import HighLight from "@components/Highlight";
 import Input from "@components/Input";
 
 export function Players() {
+
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,6 +27,25 @@ export function Players() {
         />
         <ButtonIcon icon={`add`} />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }

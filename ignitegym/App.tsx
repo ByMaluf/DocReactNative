01cffb672de76792c54d3b2b01c4b5d661420959
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-03-28 15:54:46 -0400

    SignUp Completo

--- ignitegym/App.tsx
+++ ignitegym/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/
 import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import Loading from '@components/Loading';
-import SignIn from '@screens/SignIn';
+import SignUp from '@screens/SignUp';
 
 export default function App() {
 
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent
       />
-      {fontsLoaded ? <SignIn /> : < Loading />}
+      {fontsLoaded ? <SignUp /> : < Loading />}
     </GluestackUIProvider>
   );
 }
---
+++ ignitegym/src/screens/SignUp.tsx
@@ -0,0 +1,56 @@
+import { VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
+import BackgroundImg from '@assets/background.png'
+import Logo from '../assets/logo.svg'
+import { Input } from "@components/input";
+import Button from "@components/Button";
+
+export default function SignUp() {
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w={'$full'}
+          h={624}
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt='Pessoas Treinando'
+          position="absolute"
+        />
+
+        <VStack flex={1} px={"$10"} pb="$16">
+
+          <Center my="$24">
+            <Logo />
+
+            <Text color={'$gray100'}>
+              Treine sua mente e o seu corpo.
+            </Text>
+          </Center>
+
+
+          <Center gap='$2' flex={1}>
+            <Heading color="$gray100">Crie sua Conta</Heading>
+
+
+            <Input placeholder="Nome" />
+
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+
+            <Input
+              placeholder="Senha"
+              secureTextEntry
+            />
+
+            <Button title="Criar e acessar" />
+          </Center>
+
+          <Button title="Voltar para o login" variant="outline" mt={'$12'} />
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+}

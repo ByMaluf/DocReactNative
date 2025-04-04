38f6cafc5063cdb3b84946a93b0ce622d8f40b86
Brenno Ysrael Maluf Luccas Correia <brenno.maluf@gmail.com>
2025-04-04 09:03:04 -0400

    Tab Navigation

---
+++ ignitegym/src/routes/app.routes.tsx
@@ -0,0 +1,40 @@
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { Home } from '@screens/Home';
+import { Exercise } from '@screens/Exercise';
+import { Profile } from '@screens/Profile';
+import { History } from '@screens/History';
+
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes<AppRoutes>() {
+  return (
+    <Navigator>
+      <Screen
+        name='home'
+        component={Home}
+      />
+      <Screen
+        name='history'
+        component={History}
+      />
+      <Screen
+        name='profile'
+        component={Profile}
+      />
+      <Screen
+        name='exercise'
+        component={Exercise}
+      />
+    </Navigator>
+
+  )
+}
